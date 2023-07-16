2c7e9bd63b29b5b576c4f820337382789fd91c11
Bruno Braga <brunobragaw8t@gmail.com>
2023-07-16 18:13:51 +0100

    feature: create AppButton component

---
+++ components/AppButton.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+  tag: string
+  label: string
+  loading: boolean
+}>()
+</script>
+
+<template>
+  <component :is="tag" class="btn btn-primary" :disabled="loading">
+    <div v-if="loading" class="spinner-border" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+
+    {{ label }}
+  </component>
+</template>
+
+<style scoped>
+.spinner-border {
+  width: 1rem;
+  height: 1rem;
+  margin-right: 0.5rem;
+}
+</style>
