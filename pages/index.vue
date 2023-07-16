f4276faf33800a257a4acea6e140f1064d48488a
Bruno Braga <brunobragaw8t@gmail.com>
2023-07-16 19:03:53 +0100

    feature: create Sign Up form

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'auth'
+})
+</script>
+
 <template>
-  <h1>Hello World</h1>
+  <div>
+    <h1 class="text-center mb-4">
+      Team Manager
+    </h1>
+
+    <h3 class="mb-3">
+      Sign Up
+    </h3>
+
+    <FormSignUp />
+  </div>
 </template>
