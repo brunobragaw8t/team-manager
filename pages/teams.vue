2769e4560bd9d6a7b3ea9ac96be30fd10dd45149
Bruno Braga <brunobragaw8t@gmail.com>
2023-07-17 21:58:27 +0100

    feature: create teams table and page skeleton

--- pages/teams.vue
+++ pages/teams.vue
@@ -7,17 +7,54 @@ definePageMeta({
 
 const client = useSupabaseClient<Database>()
 
-const { data: teams } = await useAsyncData('teams', async () => {
-  const { data } = await client.from('teams').select('id, name').order('created_at')
+const { data: teams } = useAsyncData('teams', async () => {
+  const { data } = await client
+    .from('teams')
+    .select('id, name')
+    .order('created_at')
 
   return data
 })
 </script>
 
 <template>
-  <ul v-if="teams">
-    <li v-for="team in teams" :key="team.id">
-      {{ team.name }}
-    </li>
-  </ul>
+  <div>
+    <div class="d-flex justify-content-between mb-4">
+      <h1 class="mb-0">
+        Teams
+      </h1>
+
+      <AppButton tag="button" label="Add team" />
+    </div>
+
+    <table v-if="teams && teams.length" class="table">
+      <thead>
+        <tr>
+          <th scope="col">
+            ID
+          </th>
+
+          <th scope="col">
+            Name
+          </th>
+
+          <th scope="col">
+            Actions
+          </th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="team in teams" :key="team.id">
+          <th scope="row">
+            {{ team.id }}
+          </th>
+
+          <td>{{ team.name }}</td>
+
+          <td>edit del</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
