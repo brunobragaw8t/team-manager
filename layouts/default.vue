d875d8ab34f3b13df6b1fb795c371692be038dc3
Bruno Braga <brunobragaw8t@gmail.com>
2023-07-24 21:19:36 +0100

    feature: add sign out button to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,32 @@
+<script setup lang="ts">
+const authClient = useSupabaseAuthClient()
+
+const signOutLoading = ref(false)
+
+async function signOut () {
+  signOutLoading.value = true
+  await authClient.auth.signOut()
+  navigateTo('/')
+}
+</script>
+
 <template>
   <div class="py-5">
     <div class="container">
       <slot />
+
+      <div class="mt-5 text-center">
+        <AppButton
+          tag="button"
+          label="Sign out"
+          icon="fas fa-sign-out"
+          icon-position="right"
+          size="sm"
+          variant="secondary"
+          :loading="signOutLoading"
+          @click="signOut"
+        />
+      </div>
     </div>
   </div>
 </template>
